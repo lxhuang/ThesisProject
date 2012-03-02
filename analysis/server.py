f18fb6eb562a4038a485906aed790538d27fc4e1
Lixing Huang <huanglx1984@hotmail.com>
2012-03-01 22:18:04 -0800

    fix bugs

--- analysis/server.py
+++ analysis/server.py
@@ -12,8 +12,8 @@ from UpdateHandler import UpdateHandler
 from UpdateHandler import NewMessageHandler
 from MarkHandler import MarkHandler
 
-define("port", default=80, type=int)
-define("mysql_host", default="127.0.0.1:3306")
+define("port", default=8483, type=int)
+define("mysql_host", default="23.21.246.188:3306")
 define("mysql_database", default="mturk")
 define("mysql_user", default="root")
 define("mysql_password", default="rhubarb")
